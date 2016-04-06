03ce9dd2b62686cec55290caf3ed133ba1948bd9
аках <@192.168.0.103>
2016-04-06 03:41:56 +0300

    добавлены 3 библиотеки для отправки email создан класс поиска котят

--- src/pcassistant/Go_Cats.java
+++ src/pcassistant/Go_Cats.java
@@ -5,36 +5,26 @@
  */
 package pcassistant;
 
-import java.net.MalformedURLException;
-import java.net.URL;
+import java.awt.Desktop;
+import java.io.File;
+import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-
 /**
  *
  * @author ТРОЛЬ Переделать
  */
 public class Go_Cats implements ICommand{
  //class myURL {
-@Override
 public void Execute() {
-
-    try {
-        URL hp = new URL("https://www.google.ru/");
-        
-        System.out. println("Protocol: " + hp.getProtocol());
-        
-        System.out.println("Port: " + hp.getPort());
-        
-        System.out.println("Host: " + hp.getHost());
-        
-        System.out.println("File: " + hp.getFile());
-        
-        System.out.println("Exit: " + hp.toExternalForm());
-    } catch (MalformedURLException ex) {
-        Logger.getLogger(Go_Cats.class.getName()).log(Level.SEVERE, null, ex);
+        try {
+             File htmlFile = new File("D:\\kittens.html");
+            Desktop.getDesktop().browse(htmlFile.toURI());
+            
+        } catch (IOException ex) {
+            System.out.println("Я не правильно отработал, мне можно а вам нельзя. Бе-бе-бе");
+        }
     }
-
-} }   
+}
 //}
 
